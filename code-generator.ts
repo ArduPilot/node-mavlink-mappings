f9dbf8c355df04199751589150f1217f58aaff78
Matthias Hryniszak <padcom@gmail.com>
2022-04-24 16:33:58 +0200

    Added storm32 and minification of dist

--- code-generator.ts
+++ code-generator.ts
@@ -452,7 +452,7 @@ function generate(name: string, obj: any, output: Writter) {
 }
 
 async function main() {
-  const parts = [ 'minimal', 'common', 'ardupilotmega', 'uavionix', 'icarous', 'asluav', 'development', /*'matrixpilot', 'paparazzi',*/ 'ualberta' ]
+  const parts = [ 'minimal', 'common', 'ardupilotmega', 'uavionix', 'icarous', 'asluav', 'development', /*'matrixpilot', 'paparazzi',*/ 'ualberta', 'storm32' ]
 
   for (let i = 0; i < parts.length; i++) {
     const part = parts[i]
---
+++ lib/storm32.imports.ts
@@ -0,0 +1,16 @@
+import {
+  uint8_t,
+  uint16_t,
+  uint32_t,
+  float
+} from './types'
+
+import {
+  MavLinkPacketRegistry,
+  MavLinkPacketField,
+  MavLinkData
+} from './mavlink'
+
+import {
+  GimbalDeviceErrorFlags
+} from './common'
